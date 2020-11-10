acfb37db8563c3569f818b9eb2ab153e6e97c2de
Mateo RH <marami26@eafit.edu.co>
2020-11-10 07:55:38 -0500

    add CDK app

--- bin/aws_pipeline.ts
+++ bin/aws_pipeline.ts
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { AwsPipelineStack } from '../lib/aws_pipeline-stack';
+import { App } from '@aws-cdk/core';
+import { LambdaStack } from '../lib/lambda-stack';
+import { PipelineStack } from '../lib/pipeline-stack';
 
-const app = new cdk.App();
-new AwsPipelineStack(app, 'AwsPipelineStack');
+const app = new App();
+
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+  env: {
+    region: 'us-west-1',
+  },
+});
+new PipelineStack(app, 'PipelineStack', {
+  lambdaCode: lambdaStack.lambdaCode,
+  githubToken: 'awsPipeline',
+  env: {
+    region: 'us-west-1',
+  },
+});
+
+app.synth();
